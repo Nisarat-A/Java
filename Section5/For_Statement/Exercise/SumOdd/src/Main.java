6e6bd5f8d2929bad9ca806962205cde0a115aaa8
Nisarat Aunrueanngam <101193445+Nisarat-A@users.noreply.github.com>
2022-10-25 23:21:51 +0700

    Add files via upload

---
+++ Section5/For_Statement/Exercise/SumOdd/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static boolean isOdd(int number){
+
+        if (number<0){
+            return false;
+        }else if(number%2==0){
+            return false;
+        } return true;
+    }
+    public static  int sumOdd(int start,int end){
+
+
+
+
+
+
+
+        return -1;
+    }
+}
