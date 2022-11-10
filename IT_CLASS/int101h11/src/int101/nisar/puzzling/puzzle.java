3b1f5379c70b2e9fcf65df4143c298c36553db0c
Nisarat Aunrueanngam <101193445+Nisarat-A@users.noreply.github.com>
2022-11-10 11:21:05 +0700

    Add files via upload

---
+++ IT_CLASS/int101h11/src/int101/nisar/puzzling/puzzle.java
@@ -0,0 +1,12 @@
+package int101.nisar.puzzling;
+
+public class puzzle {
+    private double value ;
+
+    public puzzle() {
+       // value = Math.random(); //prob uniform distribution
+    }
+    public double getValue() {
+        return Math.random();
+    }
+}
