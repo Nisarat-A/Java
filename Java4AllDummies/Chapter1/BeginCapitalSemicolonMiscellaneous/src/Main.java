56eb051926d50d96475e2886690ef2ba3be78903
Nisarat Aunrueanngam <101193445+Nisarat-A@users.noreply.github.com>
2022-11-14 19:16:58 +0700

    Add files via upload

---
+++ Java4AllDummies/Chapter1/BeginCapitalSemicolonMiscellaneous/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("You will love Java!");
+        System.out.println("No more baked beans!");
+        System.out.println("Use a Straight quote \", not a curly quote \u201D");
+
+    }
+}
---
+++ Java4AllDummies/Chapter1/Comment/src/com/nisar/Comment.java
@@ -0,0 +1,37 @@
+package com.nisar;
+/*
+ *
+ * Traditional comment
+ *
+ */
+
+//ends of line comment
+
+/**
+ *  Comment Project
+ *
+ * @author Nisarat Aunrueanngam
+ * @version 1.0 14/11/22
+ * @see java.lang.System
+ */
+public class Comment {
+    /**
+     * The main method is where
+     * execution of code begins.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("I love Java!");
+
+        /*
+        System.out.println("parents");
+        System.out.println("pick your");
+        /*
+        * Intentionally displays on four separate lines
+        */
+        System.out.println("battles");
+        System.out.println("carefully");
+       // */ aren't generate so use end of lines comment " // "
+    }
+}
